4bcdce4aa8fd9df2ab44f41f7f38198078ae5849
Louis Durieux <ldurieux@student.42lyon.fr>
2023-01-08 14:01:11 +0100

    add tilde to cd builtin

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -15,10 +15,12 @@
 #define HOME_STR "HOME="
 #define HOME_STR_LEN 5
 
-char	*get_home(char **envp)
+static char	*get_home(char **envp)
 {
 	size_t	idx;
 
+	if (!envp)
+		return (NULL);
 	idx = (size_t)-1;
 	while (envp[++idx])
 		if (ft_strncmp(envp[idx], HOME_STR, HOME_STR_LEN) == 0)
@@ -26,6 +28,18 @@ char	*get_home(char **envp)
 	if (envp[idx])
 		return (envp[idx] + HOME_STR_LEN);
 	return (NULL);
+}
+
+static char	*get_tilde(char **envp, char *next)
+{
+	char	*home;
+
+	home = get_home(envp);
+	if (!home)
+		return (NULL);
+	if (!next[0])
+		return (ft_strdup(home));
+	return (ft_strjoin(home, next));
 }
 
 static int	cd_error(char *error)
@@ -43,18 +57,19 @@ int	main_cd(int argc, char **argv, char **envp, t_ftmap *vars)
 	if (argc > 2)
 		return (cd_error("too many arguments"));
 	if (argc < 2)
-	{
-		if (envp)
-			path = get_home(envp);
-		if (!path)
-			return (cd_error("HOME not set"));
-	}
+		path = ft_strdup(get_home(envp));
+	else if (argv[1][0] == '~')
+		path = get_tilde(envp, argv[1] + 1);
 	else
-		path = argv[1];
+		path = ft_strdup(argv[1]);
+	if (!path)
+		return (cd_error("HOME not set"));
 	if (chdir(path) == -1)
 	{
 		ft_dprintf(STDERR_FILENO, "%s: %s: %s\n", "cd", strerror(errno), path);
+		free(path);
 		return (1);
 	}
+	free(path);
 	return (0);
 }
